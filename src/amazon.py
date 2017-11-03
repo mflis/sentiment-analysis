7bc2a7c583c7b96e949188d3a383bfa95c0f8403
Marcin Flis <marcin.flis.95@gmail.com>
2017-11-03 11:46:15 +0100

    experimentint with layers, dummy model not running

--- src/amazon.py
+++ src/amazon.py
@@ -7,23 +7,28 @@ from src.helpers import *
 (x_train, y_train), (x_test, y_test) = get_test_train_set()
 
 model = Sequential()
-model.add(Dense(2500, activation='sigmoid', input_dim=VOCABULARY_LIMIT))
-model.add(Dense(1, activation='softmax'))
-model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+
+# model.add(Dense(2500, activation='sigmoid', input_dim=VOCABULARY_LIMIT))
+# model.add(Dense(1, kernel_initializer='normal', activation='softmax'))
+# model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.add(Dense(VOCABULARY_LIMIT, input_dim=VOCABULARY_LIMIT,
+                kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 auc = AucMetric((x_test, y_test))
-history = model.fit(x_train, y_train, batch_size=512, epochs=2,
+history = model.fit(x_train, y_train, batch_size=4096, epochs=2,
                     validation_data=(x_test, y_test), callbacks=[auc])
 
-model.fit(x_train, y_train)
-
-score = model.evaluate(x_test, y_test, verbose=0)
+score = model.evaluate(x_test, y_test, batch_size=4096, verbose=0)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
 
 y = model.predict(x_test)
 
 print(roc_auc_score(y_test, y))
+print(np.average(y))
 
 
 
--- src/dummy_model.py
+++ src/dummy_model.py
@@ -1,5 +1,6 @@
 from sklearn.dummy import DummyClassifier
 
+from src.auc_callback import *
 from src.helpers import *
 
 (x_train, y_train), (x_test, y_test) = get_test_train_set()
@@ -7,5 +8,10 @@ model = DummyClassifier(random_state=RANDOM_SEED)
 
 trained_classifier = model.fit(x_train, y_train)
 accuracy = trained_classifier.score(x_test, y_test)
+
+y = model.predict(x_test)
+
+print(roc_auc_score(y_test, y))
+print(np.average(y))
 
 print('Test accuracy: {}'.format(accuracy))
