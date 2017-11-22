a6dad9be47530b09c4c92f7299b79870968d475d
Marcin Flis <marcin.flis.95@gmail.com>
2017-11-22 13:13:49 +0100

    implemented cnn network form paper, lowercased parameters

---
+++ src/cnn_network.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+
+from sacred import Experiment
+from tensorflow.python.keras import Input
+from tensorflow.python.keras.activations import relu
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+from tensorflow.python.keras.layers import GlobalMaxPooling2D
+from tensorflow.python.keras.layers import concatenate
+from tensorflow.python.keras.models import Model
+from tensorflow.python.layers.convolutional import Conv2D
+
+from mechanics.postprocessing.callbacks import *
+from mechanics.postprocessing.scalar_metrics import *
+from mechanics.preprocessing.read_data import *
+
+cnn_experiment = Experiment('config_demo', ingredients=[data_ingredient, loggers_ingredient])
+
+
+# todo reimplement this - http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow/
+# todo code for preprocessing https://github.com/dennybritz/cnn-text-classification-tf/blob/master/data_helpers.py
+
+# todo read-  http://papers.nips.cc/paper/5867-precision-recall-gain-curves-pr-analysis-done-right.pdf
+# todo read https://arxiv.org/abs/1510.03820 -  co warto stosować w cnn
+# przejrzec pod kątem CNN - https://www.udacity.com/course/deep-learning--ud730
+
+@data_ingredient.config
+def conf():
+    max_sequence_length = 30
+
+
+@cnn_experiment.config
+def my_config(dataset, loggers):
+    validation_split = 0.2
+    max_sequence_length = dataset['max_sequence_length']
+    embedding_dim = dataset['embedding_dim']
+    keep_prob = 0.5
+    nr_of_filters = 10
+    filter_sizes = [3, 4, 5]
+    batch_size = 128
+    epochs = 10
+
+
+@cnn_experiment.automain
+def my_main(max_sequence_length, validation_split, nr_of_filters, embedding_dim, filter_sizes, keep_prob, batch_size,
+            epochs):
+    sentences_scores, word_index, sentences = load_sentences()
+    sequence_input = Input(shape=(max_sequence_length,), dtype='int32')
+    embedding = prepare_embedding_layer(word_index=word_index)(sequence_input)
+    expanded_embedding = Lambda(lambda x: K.expand_dims(x, axis=-1))(embedding)
+
+    # todo maybe specify kernel initializers
+    convolutions = []
+    for filter_size in filter_sizes:
+        conv = Conv2D(nr_of_filters, kernel_size=(filter_size, embedding_dim), activation=relu)(expanded_embedding)
+        pool = GlobalMaxPooling2D()(conv)
+        convolutions.append(pool)
+    pooled_layers = concatenate(convolutions)
+
+    dropout = Dropout(keep_prob)(pooled_layers)
+    final_layer = Dense(1, activation='sigmoid')(dropout)  # orignal activation was argmax
+
+    model = Model(sequence_input, final_layer)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adadelta',  # as in paper, article uses adam
+                  metrics=['acc', recall, precision, roc_score])
+
+    model.fit(sentences, sentences_scores,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_split=validation_split,
+              callbacks=loggers() + checkpoints())
--- src/mechanics/preprocessing/read_data.py
+++ src/mechanics/preprocessing/read_data.py
@@ -16,28 +16,28 @@ data_ingredient = Ingredient('dataset')
 def my_config():
     sentences_file = 'balanced-reviews.csv'
     glove_file = 'glove/glove.6B.50d.txt'
-    BASE_DIR = os.path.join(ROOT_DIR, 'data')
-    sentences_path = os.path.join(BASE_DIR, sentences_file)
-    glove_path = os.path.join(BASE_DIR, '%s' % glove_file)
-    MAX_SEQUENCE_LENGTH = 1000
+    base_dir = os.path.join(ROOT_DIR, 'data')
+    sentences_path = os.path.join(base_dir, sentences_file)
+    glove_path = os.path.join(base_dir, '%s' % glove_file)
+    max_sequence_length = 100
     dictionary_limit = 20000
-    EMBEDDING_DIM = 50
+    embedding_dim = 50
     rows_cut = 100
 
 
 @data_ingredient.capture
-def load_sentences(sentences_path, rows_cut, dictionary_limit, MAX_SEQUENCE_LENGTH):
+def load_sentences(sentences_path, rows_cut, dictionary_limit, max_sequence_length):
     texts, labels = getColumns(sentences_path, rows_cut=rows_cut)
     tokenizer = Tokenizer(num_words=dictionary_limit)
     tokenizer.fit_on_texts(texts)
     sequences = tokenizer.texts_to_sequences(texts)
     word_index = tokenizer.word_index
-    data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+    data = pad_sequences(sequences, maxlen=max_sequence_length)
     return labels, word_index, data
 
 
 @data_ingredient.capture
-def prepare_embedding_layer(glove_path, word_index, dictionary_limit, EMBEDDING_DIM, MAX_SEQUENCE_LENGTH):
+def prepare_embedding_layer(glove_path, word_index, dictionary_limit, embedding_dim, max_sequence_length):
     embeddings_index = {}
     f = open(glove_path)
     for line in f:
@@ -48,7 +48,7 @@ def prepare_embedding_layer(glove_path, word_index, dictionary_limit, EMBEDDING_
     f.close()
 
     num_words = min(dictionary_limit, len(word_index)) + 1
-    embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+    embedding_matrix = np.zeros((num_words, embedding_dim))
     for word, i in word_index.items():
         if i >= num_words:
             continue
@@ -60,8 +60,8 @@ def prepare_embedding_layer(glove_path, word_index, dictionary_limit, EMBEDDING_
 
             #    note that we set trainable = False so as to keep the embeddings fixed
     embedding_layer = Embedding(input_dim=num_words,
-                                output_dim=EMBEDDING_DIM,
+                                output_dim=embedding_dim,
                                 weights=[embedding_matrix],
-                                input_length=MAX_SEQUENCE_LENGTH,
+                                input_length=max_sequence_length,
                                 trainable=False)
     return embedding_layer
