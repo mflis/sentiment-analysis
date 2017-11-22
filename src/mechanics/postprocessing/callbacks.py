8dfa170be34970208c15ca260c7e613127b68aac
Marcin Flis <marcin.flis.95@gmail.com>
2017-11-22 13:10:36 +0100

    created checkpoint callback, refactored parameters for loggers(leave only changeable)

--- src/mechanics/postprocessing/callbacks.py
+++ src/mechanics/postprocessing/callbacks.py
@@ -1,5 +1,9 @@
-from sacred import Ingredient
+import os
 
+from sacred import Ingredient
+from tensorflow.python.keras._impl.keras.callbacks import ModelCheckpoint
+
+from definitions import ROOT_DIR, CURRENT_TIME
 from mechanics.postprocessing.ConfusionMatrix import ConfusionMatrix
 from mechanics.postprocessing.ImagesTensorBoard import ImagesTensorBoard
 from mechanics.postprocessing.metric_plots import plot_roc_curve, plot_prec_recall
@@ -9,15 +13,14 @@ loggers_ingredient = Ingredient('loggers')
 
 @loggers_ingredient.config
 def conf():
-    from definitions import ROOT_DIR, CURRENT_TIME
-    import os
     log_dir = 'results/logs1'
-    train_log_dir = os.path.join(ROOT_DIR, '{}/{}-train'.format(log_dir, CURRENT_TIME))
-    val_log_dir = os.path.join(ROOT_DIR, '{}/{}-validation'.format(log_dir, CURRENT_TIME))
+    checkpoints_dir = 'results/checkpoints'
 
 
 @loggers_ingredient.capture
-def prepare_loggers(train_log_dir, val_log_dir):
+def loggers(log_dir):
+    train_log_dir = os.path.join(ROOT_DIR, '{}/{}-train'.format(log_dir, CURRENT_TIME))
+    val_log_dir = os.path.join(ROOT_DIR, '{}/{}-validation'.format(log_dir, CURRENT_TIME))
     plots = {'roc-train': plot_roc_curve,
              'prec-recall-train': plot_prec_recall,
              'confusion-metrix-train': lambda y_true, y_pred, dest: ConfusionMatrix(y_true, y_pred).write_matrix(dest)}
@@ -31,3 +34,14 @@ def prepare_loggers(train_log_dir, val_log_dir):
                                           log_dir=val_log_dir, write_graph=False,
                                           write_images=False, log_regex=r"^val")
     return [train_tboard_logger, val_tboard_logger]
+
+
+@loggers_ingredient.capture
+def checkpoints(checkpoints_dir):
+    file_partial = "{}-model".format(CURRENT_TIME)
+    checkpoint_file = file_partial + "model.{epoch:02d}-{val_loss:.2f}.hdf5"
+    checkpoint_base = os.path.join(ROOT_DIR, checkpoints_dir)
+    path = os.path.join(checkpoint_base, checkpoint_file)
+    # save_weights_only=True - workaround for problem  with saving model with lambda layer
+    # http://forums.fast.ai/t/unable-to-save-model-checkpoints-when-using-lambda-in-model/827/7
+    return [ModelCheckpoint(path, save_weights_only=True)]
