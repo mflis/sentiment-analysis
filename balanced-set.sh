524ccec5c7aa35dac192234875efbe8c00c4d8a1
Marcin Flis <marcin.flis.95@gmail.com>
2017-11-25 17:40:00 +0100

    fix balanced set creation

--- balanced-set.sh
+++ balanced-set.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cat AmazonReviews.csv  | awk '$1 <  3' | head -n 84674 > negatives.csv
-cat $1 | awk '$1 >  3' | head -n 84674 > positives.csv
+cat data/AmazonReviews.csv  | awk '$1 <  3' | head -n 84674 > negatives.csv
+cat data/AmazonReviews.csv  | awk '$1 >  3' | head -n 84674 > positives.csv
 echo "Score,Text" >> balanced-reviews.csv
 paste -d"\n" positives.csv negatives.csv    | shuf  >> balanced-reviews.csv
 rm  negatives.csv positives.csv
