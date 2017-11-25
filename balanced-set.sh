a22bf0c81ff249b5c36dac6c9d8bac20702b27a1
Marcin Flis <marcin.flis.95@gmail.com>
2017-11-25 12:05:38 +0100

    balanced-set.sh made argumentless, little cleanup

--- balanced-set.sh
+++ balanced-set.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cat $1  | awk '$1 <  3' | head -n 84674 > negatives.csv
+cat AmazonReviews.csv  | awk '$1 <  3' | head -n 84674 > negatives.csv
 cat $1 | awk '$1 >  3' | head -n 84674 > positives.csv
 echo "Score,Text" >> balanced-reviews.csv
 paste -d"\n" positives.csv negatives.csv    | shuf  >> balanced-reviews.csv
--- dowload-data.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wget  --output-document=glove.zip http://nlp.stanford.edu/data/glove.6B.zip
