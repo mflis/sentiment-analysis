ce43416e05781b564e11c8d117172869c56e9ad6
Marcin Flis <marcin.flis.95@gmail.com>
2017-12-30 09:18:30 +0100

    tfidf remove early stop

--- src/amazon-tfidf.py
+++ src/amazon-tfidf.py
@@ -3,7 +3,7 @@ from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.regularizers import l2
 
-from mechanics.postprocessing.callbacks import loggers_ingredient, earlyStop, loggers
+from mechanics.postprocessing.callbacks import loggers_ingredient, loggers
 from mechanics.preprocessing.read_data import data_ingredient, get_data_tfidf
 
 tfidf_experiment = Experiment('config_tfidf', ingredients=[data_ingredient, loggers_ingredient])
@@ -29,7 +29,7 @@ def tfidf_main(vocabulary_limit, epochs, batch_size, tag):
     model.summary()
 
     model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
-              validation_data=(x_test, y_test), callbacks=loggers(tag=tag) + earlyStop())
+              validation_data=(x_test, y_test), callbacks=loggers(tag=tag))
 
 # todo  read about statistical significance
 # todo to read: http://cs231n.github.io/neural-networks-3/
